36fcfb32b798def80d0d01c4e2366ef47db47cd1
AlexeyShok <Alexey.Shokarev@gmail.com>
2021-12-10 13:04:45 +0600

    Added storage draft

--- HappyTravel.Sunpu.Api/Services/ISupplierStorage.cs
+++ HappyTravel.Sunpu.Api/Services/ISupplierStorage.cs
@@ -1,5 +1,9 @@
-﻿namespace HappyTravel.Sunpu.Api.Services;
+﻿using HappyTravel.Sunpu.Data.Models;
+
+namespace HappyTravel.Sunpu.Api.Services;
 
 public interface ISupplierStorage
 {
+    void Add(Supplier supplier);
+    Supplier TryGet();
 }
--- HappyTravel.Sunpu.Api/Services/SupplierStorage.cs
+++ HappyTravel.Sunpu.Api/Services/SupplierStorage.cs
@@ -1,5 +1,30 @@
-﻿namespace HappyTravel.Sunpu.Api.Services;
+﻿using FloxDc.CacheFlow;
+
+namespace HappyTravel.Sunpu.Api.Services;
 
 public class SupplierStorage : ISupplierStorage
 {
+    public SupplierStorage(IMemoryFlow flow)
+    {
+        _flow = flow;
+    }
+
+
+    public void Add(string token)
+        => _flow.Set(TokenKey, token, s_tokenLifeTime);
+
+
+    public string TryGet()
+    {
+        if (_flow.TryGetValue(TokenKey, out string token))
+            return token;
+
+        return string.Empty;
+    }
+
+
+    private const string TokenKey = "Token";
+    private static readonly TimeSpan s_tokenLifeTime = TimeSpan.FromHours(24);
+
+    private readonly IMemoryFlow _flow;
 }
