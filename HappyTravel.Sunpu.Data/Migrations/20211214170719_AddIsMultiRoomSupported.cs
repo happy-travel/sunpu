c7adc5f80c8da6d668104aa14929408c603a9fd7
AlexeyShok <Alexey.Shokarev@gmail.com>
2021-12-15 13:59:07 +0600

    Added update data to migration

--- HappyTravel.Sunpu.Data/Migrations/20211214170719_AddIsMultiRoomSupported.cs
+++ HappyTravel.Sunpu.Data/Migrations/20211214170719_AddIsMultiRoomSupported.cs
@@ -14,6 +14,22 @@ namespace HappyTravel.Sunpu.Data.Migrations
                 type: "boolean",
                 nullable: false,
                 defaultValue: false);
+
+            var table = "Suppliers";
+            var keyColumn = "Id";
+            var column = "IsMultiRoomSupported";
+            migrationBuilder.UpdateData(table, keyColumn, 1, column, true); // Netstorming
+            migrationBuilder.UpdateData(table, keyColumn, 2, column, true); // iWTX
+            migrationBuilder.UpdateData(table, keyColumn, 4, column, true); // RateHawk
+            migrationBuilder.UpdateData(table, keyColumn, 7, column, true); // TravelgateX (TEST)
+            migrationBuilder.UpdateData(table, keyColumn, 8, column, true); // Jumeirah Hotels
+            migrationBuilder.UpdateData(table, keyColumn, 9, column, true); // Darina Holidays
+            migrationBuilder.UpdateData(table, keyColumn, 10, column, true); // Yalago
+            migrationBuilder.UpdateData(table, keyColumn, 11, column, true); // Paximum
+            migrationBuilder.UpdateData(table, keyColumn, 13, column, true); // Bronevik
+            migrationBuilder.UpdateData(table, keyColumn, 16, column, true); // HotelBeds
+            migrationBuilder.UpdateData(table, keyColumn, 18, column, true); // BookMe Maldives
+            migrationBuilder.UpdateData(table, keyColumn, 19, column, true); // Roibos
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
