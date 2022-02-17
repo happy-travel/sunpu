82cd54c8e19e2d904d8cf41fc7dcf54e1bf96cce
Denis Nikolaev <denis.nikolaev@happytravel.com>
2022-02-17 16:37:10 +0500

    added test connector

---
+++ HappyTravel.Sunpu.Data/Migrations/20220217103138_AddTestConnector.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HappyTravel.Sunpu.Data.Migrations
+{
+    public partial class AddTestConnector : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Suppliers", new string[] { "Id", "Code", "Name", "IsEnabled", "ConnectorUrl", "ConnectorGrpcEndpoint", "Created", "IsMultiRoomFlowSupported" }, new object[,]
+            {
+                { 33, "htTest", "HappyTravel Test Connector", false, "https://tc-dev.happytravel.com/api/1.0/", "http://tc-dev", DateTimeOffset.UtcNow, true }
+            });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("Suppliers", "Id", 33);
+        }
+    }
+}
