f43a8cd3ef9d1fa342161710e311157094b0c2f5
mingazhev <43397444+mingazhev@users.noreply.github.com>
2022-03-03 12:21:07 +0500

    Added redis

--- HappyTravel.Sunpu.Api/Infrastructure/ConfigureExtensions/ConfigureServicesExtension.cs
+++ HappyTravel.Sunpu.Api/Infrastructure/ConfigureExtensions/ConfigureServicesExtension.cs
@@ -1,26 +1,31 @@
 ﻿using FloxDc.CacheFlow.Extensions;
 using HappyTravel.ErrorHandling.Extensions;
+using HappyTravel.Sunpu.Api.Infrastructure.Environment;
 using HappyTravel.Sunpu.Api.Services;
 
 namespace HappyTravel.Sunpu.Api.Infrastructure.ConfigureExtensions;
 
 public static class ConfigureServicesExtension
 {
-    public static void ConfigureServices(this WebApplicationBuilder builder)
+    public static void ConfigureServices(this WebApplicationBuilder builder, IConfiguration configuration)
     {
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-        builder.Services.AddHealthChecks();
+        builder.Services.AddHealthChecks().AddRedis(EnvironmentVariableHelper.Get("Redis:Endpoint", configuration));
         builder.Services.AddProblemDetailsErrorHandling();
         builder.Services.AddResponseCompression();
         builder.Services.AddMemoryCache();
-        builder.Services.AddMemoryFlow();
+        builder.Services.AddMemoryFlow().AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = EnvironmentVariableHelper.Get("Redis:Endpoint", configuration);
+        });
         builder.Services.ConfigureApiVersioning();
         builder.Services.ConfigureTracing(builder.Environment, builder.Configuration);
         builder.Services.ConfigureFlowOptions();
         builder.Services.ConfigureSwagger();
         builder.Services.AddTransient<ISupplierService, SupplierService>();
         builder.Services.AddTransient<ISupplierStorage, SupplierStorage>();
+        
     }
 }
