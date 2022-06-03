a17d1970a47dd9ab90c66f227dcce0cb8bd4b3d3
Denis Nikolaev <denis.nikolaev@happytravel.tech>
2022-06-03 16:25:32 +0500

    added ability to activate gRPC

---
+++ HappyTravel.Sunpu.Data/Migrations/20220603112057_AddCanUseGrpcColumn.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HappyTravel.Sunpu.Data.Migrations
+{
+    public partial class AddCanUseGrpcColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CanUseGrpc",
+                table: "Suppliers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CanUseGrpc",
+                table: "Suppliers");
+        }
+    }
+}
