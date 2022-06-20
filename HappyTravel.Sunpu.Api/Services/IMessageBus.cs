007b3256e19e96ca3838108a0eff00439d3cb28f
Denis Nikolaev <dfs.str@gmail.com>
2022-06-20 16:00:30 +0500

    added xml documentation

--- HappyTravel.Sunpu.Api/Services/IMessageBus.cs
+++ HappyTravel.Sunpu.Api/Services/IMessageBus.cs
@@ -2,5 +2,11 @@ namespace HappyTravel.Sunpu.Api.Services;
 
 public interface IMessageBus
 {
+    /// <summary>
+    /// Sent message to message bus
+    /// </summary>
+    /// <param name="topicName">Topic name</param>
+    /// <param name="message">Message object</param>
+    /// <typeparam name="T">Message type</typeparam>
     void Publish<T>(string topicName, T message);
 }
