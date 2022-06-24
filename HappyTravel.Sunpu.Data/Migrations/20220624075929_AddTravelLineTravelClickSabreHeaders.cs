9ff8deab4848498f71bd0568f532904504bd0d8b
Denis Nikolaev <dfs.str@gmail.com>
2022-06-24 13:09:52 +0500

    added custom headers to open travel suppliers

---
+++ HappyTravel.Sunpu.Data/Migrations/20220624075929_AddTravelLineTravelClickSabreHeaders.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HappyTravel.Sunpu.Data.Migrations
+{
+    public partial class AddTravelLineTravelClickSabreHeaders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(table: "Suppliers", 
+                keyColumn: "Code", 
+                keyValue: "travelLine", 
+                column: "CustomHeaders",  
+                value: new Dictionary<string, string> {{ "Supplier-Context", "travelLine" }});
+            
+            migrationBuilder.UpdateData(table: "Suppliers", 
+                keyColumn: "Code", 
+                keyValue: "travelClick", 
+                column: "CustomHeaders",  
+                value: new Dictionary<string, string> {{ "Supplier-Context", "travelClick" }});
+            
+            migrationBuilder.UpdateData(table: "Suppliers", 
+                keyColumn: "Code", 
+                keyValue: "sabre", 
+                column: "CustomHeaders",  
+                value: new Dictionary<string, string> {{ "Supplier-Context", "synexis" }});
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(table: "Suppliers", 
+                keyColumn: "Code", 
+                keyValue: "travelLine", 
+                column: "CustomHeaders",  
+                value: null);
+            
+            migrationBuilder.UpdateData(table: "Suppliers", 
+                keyColumn: "Code", 
+                keyValue: "travelClick", 
+                column: "CustomHeaders",  
+                value: null);
+            
+            migrationBuilder.UpdateData(table: "Suppliers", 
+                keyColumn: "Code", 
+                keyValue: "sabre", 
+                column: "CustomHeaders",  
+                value: null);
+        }
+    }
+}
