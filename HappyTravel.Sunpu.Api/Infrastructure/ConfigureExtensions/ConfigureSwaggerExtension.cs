94f071b6d257283f1c185f4d0647577e6d87c159
AlexeyShok <Alexey.Shokarev@gmail.com>
2022-06-29 09:11:15 +0600

    Added ability to login in sunpu from swagger

--- HappyTravel.Sunpu.Api/Infrastructure/ConfigureExtensions/ConfigureSwaggerExtension.cs
+++ HappyTravel.Sunpu.Api/Infrastructure/ConfigureExtensions/ConfigureSwaggerExtension.cs
@@ -14,6 +14,31 @@ public static class ConfigureSwaggerExtension
             var xmlCommentsFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
             var xmlCommentsFilePath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFileName);
             options.IncludeXmlComments(xmlCommentsFilePath);
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+                    },
+                    Array.Empty<string>()
+                }
+            });
         });
     }
 }
